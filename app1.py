0d46f51d6c16c7450a65f53de2c3008679d5a464
kirilig09 <kirilig1809@abv.bg>
2020-03-06 15:59:04 +0200

    copied from previous flask project

--- app1.py
+++ app1.py
@@ -26,3 +26,43 @@ def main():
 @auth.login_required
 def chat():
     return render_template("chat.html", username=auth.username(), rooms = ROOMS)
+
+@app.route("/api/users", methods=["POST"])
+def create_user():
+    user_data = request.get_json(force=True, silent=True)
+    if user_data == None:
+        return "Bad request", 401
+    hashed_password = generate_password_hash(user_data["password"])
+    user = User(user_data["name"], hashed_password)
+    user.save()
+    return jsonify(user.to_dict()), 201
+
+
+@app.route("/api/users/<user_id>", methods=["GET"])
+def get_user(user_id):
+    return jsonify(User.find(user_id).to_dict())
+
+
+@app.route("/api/users", methods=["GET"])
+def list_users():
+    result = {"result": []}
+    for user in User.all():
+        result["result"].append(user.to_dict())
+    return jsonify(result), 201
+
+
+@app.route("/api/users/<user_id>", methods=["PATCH"])
+def update_user(user_id):
+    user_data = request.get_json(force=True, silent=True)
+    if user_data == None:
+        return "Bad request", 401
+
+    user = User.find(user_id)
+    return jsonify(user.save().to_dict()), 201
+
+
+@app.route("/api/users/<user_id>", methods=["DELETE"])
+def delete_user(user_id):
+    user = User.find(user_id)
+    user.delete(user_id)
+    return ""

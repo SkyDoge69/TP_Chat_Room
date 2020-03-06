ecb6714fb28355ac7e73ef1e8501f6ff4c2c96d8
kirilig09 <kirilig1809@abv.bg>
2020-03-06 14:43:32 +0200

    imported used libraries and such

--- app1.py
+++ app1.py
@@ -1,1 +1,18 @@
-#yes
+from time import localtime, strftime
+
+from flask import Flask, request, render_template, jsonify
+from flask_socketio import SocketIO, send, emit, join_room, leave_room
+
+import json
+import uuid
+
+from model.user import User
+from errors import register_error_handlers
+
+from security.basic_authentication import generate_password_hash, init_basic_auth
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+app = Flask(_name_)
+auth = init_basic_auth()
+register_error_handlers(app)

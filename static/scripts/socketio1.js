c1ae356e2c8ffead790b66f2050c4ac1948c8249
SkyDoge69 <luc2002penta@gmail.com>
2020-03-08 20:22:27 +0200

    First socket tests, structured chat.html

--- static/scripts/socketio1.js
+++ static/scripts/socketio1.js
@@ -1,1 +1,11 @@
-//yes
+document.addEventListener('DOMContentLoaded', () => {
+    var socket = io.connect('http://' + document.domain + ':' + location.port);
+
+    socket.on('connect', () => {
+        socket.send("I am connected");
+    })
+
+    socket.on('message', data => {
+        console.log(`Message recieved: ${data}`)
+    }) 
+})

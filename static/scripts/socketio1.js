42e964a9e88126c426d3833c736f00b25a5436f3
kirilig09 <kirilig1809@abv.bg>
2020-03-09 17:09:50 +0200

    Adding msg display

--- static/scripts/socketio1.js
+++ static/scripts/socketio1.js
@@ -1,11 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     var socket = io.connect('http://' + document.domain + ':' + location.port);
 
-    socket.on('connect', () => {
-        socket.send("I am connected");
-    })
-
     socket.on('message', data => {
-        console.log(`Message recieved: ${data}`)
-    }) 
-})
+        const p = document.createElement('p');
+        const span_username = document.createElement('span');
+        const span_timestamp = document.createElement('span');
+        const br = document.createElement('br');
+        
+        if (data.username) {
+            span_username.innerHTML = data.username;
+            span_timestamp.innerHTML = data.time_stamp;
+            p.innerHTML = span_username.outerHTML + br.outerHTML + data.msg + br.outerHTML + span_timestamp.outerHTML;
+            document.querySelector('#display-message-section').append(p);
+        } else {
+            printSysMsg(data.msg);
+        } 
+});

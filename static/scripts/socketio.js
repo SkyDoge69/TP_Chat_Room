7a8ac1cb43e0a9d6637ae0080c3d36a532c8af2d
SkyDoge69 <luc2002penta@gmail.com>
2020-03-11 19:59:55 +0200

    Last formatting change before milestone1, renamed

---
+++ static/scripts/socketio.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', () => {
+    var socket = io.connect('http://' + document.domain + ':' + location.port);
+
+    let room = "Lounge";
+    joinRoom("Lounge");
+
+    socket.on('message', data => {
+        const p = document.createElement('p');
+        const span_username = document.createElement('span');
+        const span_timestamp = document.createElement('span');
+        const br = document.createElement('br');
+        
+        if (data.username) {
+            span_username.innerHTML = data.username;
+            span_timestamp.innerHTML = data.time_stamp;
+            p.innerHTML = span_username.outerHTML + br.outerHTML + data.msg + br.outerHTML + span_timestamp.outerHTML;
+            document.querySelector('#display-message-section').append(p);
+        } else {
+            printSysMsg(data.msg);
+        }
+    });
+
+    
+    document.querySelector('#send_message').onclick = () => {
+        socket.send({'msg': document.querySelector('#user_message').value, 
+        'username': username, 'room': room});
+        document.querySelector('#user_message').value = '';
+    }
+    
+    document.querySelectorAll('.select-room').forEach(p => {
+        p.onclick = () => {
+            let newRoom = p.innerHTML;
+            if (newRoom == room) {
+                msg = `You are already in ${room} room.`
+            } else {
+                leaveRoom(room);
+                joinRoom(newRoom);
+                room = newRoom;
+            }
+        }
+
+    });
+
+    function leaveRoom(room) {
+        socket.emit('leave', {'username' : username, 'room': room});
+    }
+
+    function joinRoom(room) {
+        socket.emit('join', {'username': username, 'room': room});
+         document.querySelector('#display-message-section').innerHTML = '';
+    }
+
+    function printSysMsg(msg) {
+        const p = document.createElement('p');
+        p.innerHTML = msg;
+        document.querySelector('#display-message-section').append(p);
+    }
+
+})

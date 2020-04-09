522db8ca38c1a18fdb04fa20a3e0f76367eca552
SkyDoge69 <luc2002penta@gmail.com>
2020-04-09 18:33:22 +0300

    Merge:

--- model/user.py
+++ model/user.py
@@ -1,27 +1,27 @@
 from database import SQLite
 from errors import ApplicationError
-
-
+ 
+ 
 class User(object):
-
+ 
     def __init__(self, name, password, room, user_id=None):
         self.id = user_id
         self.name = name
         self.password = password
         self.room = room
-
-      
+ 
+     
     def to_dict(self):
         user_data = self.__dict__
         del user_data["password"]
         return user_data
-
+ 
     def save(self):
         with SQLite() as db:
             cursor = db.execute(self.__get_save_query())
             self.id = cursor.lastrowid
         return self
-
+ 
     @staticmethod
     def delete(user_id):
         result = None
@@ -30,7 +30,7 @@ class User(object):
                     (user_id,))
         if result.rowcount == 0:
             raise ApplicationError("No value present", 404)
-
+ 
     @staticmethod
     def find(user_id):
         result = None
@@ -43,7 +43,7 @@ class User(object):
             raise ApplicationError(
                     "User with id {} not found".format(user_id), 404)
         return User(*user)
-
+ 
     @staticmethod
     def find_by_name(name):
         result = None
@@ -56,14 +56,25 @@ class User(object):
             raise ApplicationError(
                     "User with name {} not found".format(name), 404)
         return User(*user)
-
+ 
+ 
+    @staticmethod
+    def update_room(room, name):
+        result = None
+        with SQLite() as db:
+            result = db.execute("UPDATE user SET room = ? WHERE name = ?",
+                    (room, name,))
+        if result.rowcount == 0:
+            raise ApplicationError("No user present", 404)
+ 
+ 
     @staticmethod
     def all():
         with SQLite() as db:
             result = db.execute(
                     "SELECT name, password, room, id FROM user").fetchall()
             return [User(*row) for row in result]
-
+ 
     def __get_save_query(self):
         query = "{} INTO user {} VALUES {}"
         if self.id == None:
@@ -73,5 +84,3 @@ class User(object):
             args = (self.id, self.name, self.password, self.room)
             query = query.format("REPLACE", "(id, name, password, room)", args)
         return query
-
-
